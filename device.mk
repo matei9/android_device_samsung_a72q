60b95bf8615055b4a33b52520fed5e0a583b3be0
TestaMic <mateidiac9@gmail.com>
2024-11-05 17:24:00 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,3 +31,5 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+#Add UDFPS customizations
+TARGET_HAS_UDFPS := true
