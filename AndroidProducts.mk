d165f88a5fbdafa985eb6d6c85a449f1f9f35bd7
TestaMic <mateidiac9@gmail.com>
2025-06-17 18:27:07 +0300

    Inital cAOSP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The aospOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a72q.mk
+    $(LOCAL_DIR)/aosp_a72q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a72q-userdebug \
-    lineage_a72q-user \
-    lineage_a72q-eng
+    aosp_a72q-userdebug \
+    aosp_a72q-user \
+    aosp_a72q-eng
